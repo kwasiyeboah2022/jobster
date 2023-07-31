0695f0b760a4d2abbcfd09b10947087d52c4754c
kwasiyeboah2022 <akuamoahyaw2015@gmail.com>
2023-07-31 14:59:56 -0700

    changes in register js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -85,7 +85,7 @@ const Register = () => {
         <button type="submit" className="btn btn-block" disabled={isLoading}>
           {isLoading ? 'loading...' : 'submit'}
         </button>
-        <button
+        {/* <button
           type="button"
           className="btn btn-block btn-hipster"
           disabled={isLoading}
@@ -96,7 +96,7 @@ const Register = () => {
           }
         >
           {isLoading ? 'loading...' : 'demo app'}
-        </button>
+        </button> */}
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
           <button type="button" onClick={toggleMember} className="member-btn">
